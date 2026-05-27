8be478a4b7e4bcf2f8d81c1199289e94a5fa79c4
Ivan Araujo <isaraujo14@gmail.com>
2026-05-27 15:06:27 -0400

    AtualizacaoSite
    Atualização do site

---
+++ js/javascript.js
@@ -0,0 +1,34 @@
+﻿// HEADER ROLAGEM
+
+window.addEventListener('scroll', () => {
+
+    const header = document.getElementById('header');
+
+    header.classList.toggle('rolagem', window.scrollY > 50);
+
+});
+
+// MENU MOBILE
+
+const menuMobile = document.getElementById('menu-mobile');
+const menu = document.getElementById('menu');
+
+menuMobile.addEventListener('click', () => {
+
+    menu.classList.toggle('active');
+
+    menuMobile.classList.toggle('open');
+
+});
+
+// FECHAR MENU AO CLICAR
+
+document.querySelectorAll('nav a').forEach(link => {
+
+    link.addEventListener('click', () => {
+
+        menu.classList.remove('active');
+
+    });
+
+});
